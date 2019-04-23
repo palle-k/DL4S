d7597f7ed8a31c892a4ad78225d8317c15196bf3
Palle Klewitz <palle.klewitz@tum.de>
2019-04-23 20:54:05 +0200

    log writing

---
+++ Sources/DL4S/SummaryWriter.swift
@@ -0,0 +1,25 @@
+//
+//  SummaryWriter.swift
+//  DL4S
+//
+//  Created by Palle Klewitz on 23.04.19.
+//
+
+import Foundation
+
+
+public class SummaryWriter {
+    private let eventsFile: FileHandle
+    
+    public init(destination: URL) throws {
+        let eventsFile = destination.appendingPathComponent("events.csv")
+        self.eventsFile = try FileHandle(forWritingTo: eventsFile)
+        self.eventsFile.seekToEndOfFile()
+    }
+    
+    public func write<Scalar: NumericType>(_ scalar: Scalar, named name: String, at iteration: Int) {
+        let milliseconds = Int(Date().timeIntervalSince1970 * 1000)
+        self.eventsFile.write("\(iteration),\(milliseconds),\(name),\(scalar)")
+        self.eventsFile.synchronizeFile()
+    }
+}
