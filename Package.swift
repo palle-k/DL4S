2a4425acf27bafbb3fb62d0c3cf5cd4b7e30708e
Palle Klewitz <palle.klewitz@tum.de>
2019-11-05 20:44:35 +0100

    minor stuff

--- Package.swift
+++ Package.swift
@@ -8,21 +8,16 @@ let package = Package(
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13),
-        .tvOS(.v13)
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DL4S",
             targets: ["DL4S", "MKL"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MKL",
             dependencies: []),
