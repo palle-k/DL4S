8f4fcc60935a0ae63bbc334e7d5b5c308fe3092f
Palle Klewitz <palleklewitz@icloud.com>
2020-06-21 03:13:22 +0200

    Adds fallback paths

--- util/debugger_support/tensor.py
+++ util/debugger_support/tensor.py
@@ -4,9 +4,20 @@ def describe_tensor(value, internal_dict):
     stream = lldb.SBStream()
     value.GetExpressionPath(stream)
     expr = stream.GetData() + ".debugDescription"
-    val = value.GetTarget().EvaluateExpression(expr)
-    return str(val.GetSummary()).strip().strip('"').replace("\\n", "\n")
     
+    val = value.GetTarget().EvaluateExpression(expr)
+    if val.GetError().GetCString() is None:
+        return str(val.GetSummary()).strip().strip('"').replace("\\n", "\n")
+    
+    val = value.GetFrame().EvaluateExpression(expr)
+    if val.GetError().GetCString() is None:
+        return str(val.GetSummary()).strip().strip('"').replace("\\n", "\n")
+    
+    val = value.CreateValueFromExpression("x", expr)
+    if val.GetError().GetCString() is None:
+        return str(val.GetSummary()).strip().strip('"').replace("\\n", "\n")
+        
+    return "could not generate description"
 
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand("type summary add -F " + __name__ + '.describe_tensor -x "DL4S\\.Tensor<.+>" -w swift')
