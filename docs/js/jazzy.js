64be55a4cb5232c48eabbbc789cb7ceada8cc8b9
Palle Klewitz <palleklewitz@icloud.com>
2021-10-25 04:05:22 +0200

    Doc rebuild

--- docs/js/jazzy.js
+++ docs/js/jazzy.js
@@ -1,4 +1,4 @@
-window.jazzy = { 'docset': false }
+window.jazzy = {'docset': false}
 if (typeof window.dash != 'undefined') {
   document.documentElement.className += ' dash'
   window.jazzy.docset = true
@@ -34,7 +34,7 @@ $(openCurrentItemIfClosed);
 $(window).on('hashchange', openCurrentItemIfClosed);
 
 // On item link ('token') click, toggle its discussion
-$('.token').on('click', function (event) {
+$('.token').on('click', function(event) {
   if (window.jazzy.docset) {
     return;
   }
@@ -52,7 +52,7 @@ $('.token').on('click', function (event) {
 });
 
 // Clicks on links to the current, closed, item need to open the item
-$("a:not('.token')").on('click', function () {
+$("a:not('.token')").on('click', function() {
   if (location == this.href) {
     openCurrentItemIfClosed();
   }
@@ -60,7 +60,7 @@ $("a:not('.token')").on('click', function () {
 
 // KaTeX rendering
 if ("katex" in window) {
-  $($('.math').each((_, element) => {
+  $($('.math').each( (_, element) => {
     katex.render(element.textContent, element, {
       displayMode: $(element).hasClass('m-block'),
       throwOnError: false,
--- docs/js/other.js
+++
@@ -1,4 +0,0 @@
-$(function () {
-	console.log("HI");
-	$(".navigation").load("./navbar.html");
-});
